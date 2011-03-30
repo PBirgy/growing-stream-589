443e3afcee6d27df03c4534b25f830506d2c0d75
unknown <Paul@.(none)>
2011-03-29 23:38:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowingStream589::Application.config.secret_token = 'a29715b000f2a23e1ec8f4f9b029e9539e91fbefedc14c5dc4fbe84586855bff4a86f7cfa331c19b51be8860283aa76af9b21a0e078d02bf843a4cbdc9b0c42e'
